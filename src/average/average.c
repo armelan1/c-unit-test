bebf2cc826a37035332d14d548f10f3c8d1c2456
Austin Melancon <armelan1@asu.edu>
2025-07-02 12:31:07 -0700

    fixed average.c to pass unit test (null array test)

--- src/average/average.c
+++ src/average/average.c
@@ -1,10 +1,10 @@
 #include "average.h"
 
 float average(float arr[], unsigned int size) {
-    // if (size == 0)
-    // {
-    //     return 0.0;
-    // }
+    if (size == 0)
+    {
+        return 0.0;
+    }
 
     float sum = 0;
 
